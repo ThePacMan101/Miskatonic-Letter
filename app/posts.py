2a3aee7187d1904810b20378acf5c4d8b7e5c521
ThePacMan101 <lulalucas.27@gmail.com>
2025-09-07 06:11:39 -0300

    Completes migration to SQLite

--- app/posts.py
+++ app/posts.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter,Request,Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from db import get_db,serialize_post_face,Posts, Users, Badges, Tags, PostsTags
+from db import Posts, Users, Badges, Tags, PostsTags
+from db import get_db,serialize_post_face,serialize_post_content
 from sqlalchemy.orm import Session
 from sqlalchemy import or_
 
@@ -11,7 +12,7 @@ router = APIRouter(tags=['Posts'])
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/Posts", response_class=HTMLResponse)
-async def get_posts(request: Request, page: int = 1,q:str = "",db:Session = Depends(get_db)):
+async def get_posts(request: Request, page: int = 1,q:str = "",db:Session = Depends(get_db),sort:str = "Newest"):
     POSTS_PER_PAGE = 5
     posts = db.query(Posts)
     if q:
@@ -28,7 +29,7 @@ async def get_posts(request: Request, page: int = 1,q:str = "",db:Session = Depe
                 Users.user_name.ilike(q_lower),
                 Tags.tag_lable.ilike(q_lower)
             )
-        )
+        ).distinct()
     total_posts = posts.count()
     total_pages = (total_posts + POSTS_PER_PAGE - 1) // POSTS_PER_PAGE
     page_posts  = posts.offset((page-1)*POSTS_PER_PAGE).limit(POSTS_PER_PAGE).all()
@@ -45,56 +46,14 @@ async def get_posts(request: Request, page: int = 1,q:str = "",db:Session = Depe
     )
 
 @router.get("/Posts/{id}", response_class=HTMLResponse)
-async def get_post(request: Request, id: str):
-    with open("data/posts.json", "r", encoding="utf-8") as f:
-        posts = json.load(f)
-    post = next((p for p in posts if p.get("id") == id), None)
-    page = request.query_params.get("page", 1)
-    q = request.query_params.get("q", "")
-    return templates.TemplateResponse("post.html", {"request": request, "post": post, "page": page, "q": q})
-
-
-
-# @router.get("/Posts", response_class=HTMLResponse)
-# async def get_posts(request: Request, page: int = 1,q:str = ""):
-#     POSTS_PER_PAGE = 4
-#     with open("data/posts.json", "r", encoding="utf-8") as f:
-#         posts = json.load(f)
-#     if q:
-#         q_lower = q.lower()
-#         def matches(post):
-#             title = post.get("title","").lower()
-#             author = post.get("author",{}).get("name","").lower()
-#             content = " ".join(post.get("content",[])).lower()
-#             tags = [tag.lower() for tag in post.get("tags",[])]
-#             return (
-#                 q_lower in title 
-#                 or q_lower in author 
-#                 or q_lower in content 
-#                 or any(q_lower in tag for tag in tags)
-#             )
-#         posts = [p for p in posts if matches(p)]
-#     total_pages = (len(posts) + POSTS_PER_PAGE - 1) // POSTS_PER_PAGE
-#     start = (page - 1) * POSTS_PER_PAGE
-#     end = start + POSTS_PER_PAGE
-#     page_posts = posts[start:end]
-#     return templates.TemplateResponse(
-#         "index.html",
-#         {
-#             "request": request,
-#             "posts": page_posts,
-#             "page": page,
-#             "total_pages": total_pages,
-#             "q":q
-#         }
-#     )
-
-# @router.get("/Posts/{id}", response_class=HTMLResponse)
-# async def get_post(request: Request, id: str):
-#     with open("data/posts.json", "r", encoding="utf-8") as f:
-#         posts = json.load(f)
-#     post = next((p for p in posts if p.get("id") == id), None)
-#     page = request.query_params.get("page", 1)
-#     q = request.query_params.get("q", "")
-#     return templates.TemplateResponse("post.html", {"request": request, "post": post, "page": page, "q": q})
+async def get_post(request: Request, id: str,db:Session = Depends(get_db)):
+    post = db.query(Posts).filter(Posts.post_id==id).first()
+    serialized_post = serialize_post_content(post,db)
+    return templates.TemplateResponse(
+        "post.html", 
+        {
+            "request": request, 
+            "post": serialized_post, 
+        }
+    )
 
