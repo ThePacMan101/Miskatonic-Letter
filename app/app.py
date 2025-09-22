04d5aada2f1677915f157a60ec6b96a40d44d0d8
ThePacMan101 <lulalucas.27@gmail.com>
2025-09-22 17:20:18 -0300

    Adds login page with reference button linking to it on index.html

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import posts
+import login
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(posts.router)
+app.include_router(login.router)
 
 
